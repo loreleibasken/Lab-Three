24bfad2e415e15ae855857db432ad512e06ff5fa
loreleibasken <lbasken@student.dwc.edu>
2015-03-05 16:06:29 -0800

    added bmp images

---
+++ Lab3_TicTacToe/Lab3_TicTacToe/Cell.h
@@ -0,0 +1,15 @@
+using namespace System::Windows::Forms;
+using namespace System::Drawing;
+
+ref class cell {
+
+private:
+
+
+
+
+public:
+
+
+
+};
--- Lab3_TicTacToe/Lab3_TicTacToe/MyForm.h
+++ Lab3_TicTacToe/Lab3_TicTacToe/MyForm.h
@@ -34,6 +34,22 @@ namespace Project1 {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Button^  button1;
+	protected:
+	private: System::Windows::Forms::Button^  button2;
+	private: System::Windows::Forms::Button^  button3;
+	private: System::Windows::Forms::Button^  button4;
+	private: System::Windows::Forms::Button^  button5;
+	private: System::Windows::Forms::Button^  button6;
+	private: System::Windows::Forms::Button^  button7;
+	private: System::Windows::Forms::Button^  button8;
+	private: System::Windows::Forms::Button^  button9;
+	private: System::Windows::Forms::MenuStrip^  menuStrip1;
+	private: System::Windows::Forms::ToolStripMenuItem^  aboutToolStripMenuItem;
+
+
+
+
 
 	private:
 		/// <summary>
@@ -48,18 +64,142 @@ namespace Project1 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->button4 = (gcnew System::Windows::Forms::Button());
+			this->button5 = (gcnew System::Windows::Forms::Button());
+			this->button6 = (gcnew System::Windows::Forms::Button());
+			this->button7 = (gcnew System::Windows::Forms::Button());
+			this->button8 = (gcnew System::Windows::Forms::Button());
+			this->button9 = (gcnew System::Windows::Forms::Button());
+			this->menuStrip1 = (gcnew System::Windows::Forms::MenuStrip());
+			this->aboutToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->menuStrip1->SuspendLayout();
 			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(12, 138);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(60, 47);
+			this->button1->TabIndex = 0;
+			this->button1->UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(95, 138);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(61, 47);
+			this->button2->TabIndex = 1;
+			this->button2->UseVisualStyleBackColor = true;
+			// 
+			// button3
+			// 
+			this->button3->Location = System::Drawing::Point(185, 138);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(61, 47);
+			this->button3->TabIndex = 2;
+			this->button3->UseVisualStyleBackColor = true;
+			// 
+			// button4
+			// 
+			this->button4->Location = System::Drawing::Point(12, 213);
+			this->button4->Name = L"button4";
+			this->button4->Size = System::Drawing::Size(60, 47);
+			this->button4->TabIndex = 3;
+			this->button4->UseVisualStyleBackColor = true;
+			// 
+			// button5
+			// 
+			this->button5->Location = System::Drawing::Point(95, 213);
+			this->button5->Name = L"button5";
+			this->button5->Size = System::Drawing::Size(61, 47);
+			this->button5->TabIndex = 4;
+			this->button5->UseVisualStyleBackColor = true;
+			// 
+			// button6
+			// 
+			this->button6->Location = System::Drawing::Point(185, 213);
+			this->button6->Name = L"button6";
+			this->button6->Size = System::Drawing::Size(61, 47);
+			this->button6->TabIndex = 5;
+			this->button6->UseVisualStyleBackColor = true;
+			// 
+			// button7
+			// 
+			this->button7->Location = System::Drawing::Point(12, 284);
+			this->button7->Name = L"button7";
+			this->button7->Size = System::Drawing::Size(60, 49);
+			this->button7->TabIndex = 6;
+			this->button7->UseVisualStyleBackColor = true;
+			// 
+			// button8
+			// 
+			this->button8->Location = System::Drawing::Point(95, 284);
+			this->button8->Name = L"button8";
+			this->button8->Size = System::Drawing::Size(61, 47);
+			this->button8->TabIndex = 7;
+			this->button8->UseVisualStyleBackColor = true;
+			// 
+			// button9
+			// 
+			this->button9->Location = System::Drawing::Point(185, 284);
+			this->button9->Name = L"button9";
+			this->button9->Size = System::Drawing::Size(61, 47);
+			this->button9->TabIndex = 8;
+			this->button9->UseVisualStyleBackColor = true;
+			this->button9->Click += gcnew System::EventHandler(this, &MyForm::button9_Click);
+			// 
+			// menuStrip1
+			// 
+			this->menuStrip1->Items->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(1) { this->aboutToolStripMenuItem });
+			this->menuStrip1->Location = System::Drawing::Point(0, 0);
+			this->menuStrip1->Name = L"menuStrip1";
+			this->menuStrip1->Size = System::Drawing::Size(261, 28);
+			this->menuStrip1->TabIndex = 9;
+			this->menuStrip1->Text = L"menuStrip1";
+			// 
+			// aboutToolStripMenuItem
+			// 
+			this->aboutToolStripMenuItem->Name = L"aboutToolStripMenuItem";
+			this->aboutToolStripMenuItem->Size = System::Drawing::Size(62, 24);
+			this->aboutToolStripMenuItem->Text = L"About";
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(665, 492);
+			this->ClientSize = System::Drawing::Size(261, 367);
+			this->Controls->Add(this->button9);
+			this->Controls->Add(this->button8);
+			this->Controls->Add(this->button7);
+			this->Controls->Add(this->button6);
+			this->Controls->Add(this->button5);
+			this->Controls->Add(this->button4);
+			this->Controls->Add(this->button3);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->menuStrip1);
+			this->MainMenuStrip = this->menuStrip1;
+			this->MaximizeBox = false;
 			this->Name = L"MyForm";
-			this->Text = L"MyForm";
+			this->Text = L"Tic Tac Toe";
+			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
+			this->menuStrip1->ResumeLayout(false);
+			this->menuStrip1->PerformLayout();
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
-	};
+	private: System::Void MyForm_Load(System::Object^  sender, System::EventArgs^  e) {
+	}
+	private: System::Void button9_Click(System::Object^  sender, System::EventArgs^  e) {
+	}
+private: System::Void toolStrip1_ItemClicked(System::Object^  sender, System::Windows::Forms::ToolStripItemClickedEventArgs^  e) {
+}
+private: System::Void toolStripButton1_Click(System::Object^  sender, System::EventArgs^  e) {
+}
+};
 }
